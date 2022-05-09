29af945b14571b7a7dd3e4bbab4edc5760704c04
MS-22040858206\Administrator <lindehao@4399.com>
2022-05-09 09:41:58 +0800

    fix: eslint

--- src/network/request.js
+++ src/network/request.js
@@ -1,35 +1,35 @@
 import axios from 'axios';
 
 export function request(config) {
-  //创建axios实例
-  const axios1 = axios.create({
+    // 创建axios实例
+    const axios1 = axios.create({
     // baseURL: 'http://localhost:3000',
-    baseURL: 'http://47.107.99.40:3000',
-    withCredentials: true,
-  });
-  // 2.1.请求拦截的作用
-  axios1.interceptors.request.use(
-    (config) => {
-      // console.log('requestconfig',config)
-      return config;
-    },
-    (err) => {
-      console.log(err);
-    }
-  );
-  // 2.2.响应拦截
-  axios1.interceptors.response.use(
-    (res) => {
-      // console.log('responsedata', res)
-      return res.data;
-    },
-    (err) => {
-      console.log(err);
-      // Dialog.alert({
-      //     message:'账户不存在！',
-      // })
-    }
-  );
-  // 3.发送真正的网络请求
-  return axios1(config);
+        baseURL: 'http://47.107.99.40:3000',
+        withCredentials: true
+    });
+    // 2.1.请求拦截的作用
+    axios1.interceptors.request.use(
+        (config) => {
+            // console.log('requestconfig',config)
+            return config;
+        },
+        (err) => {
+            console.log(err);
+        }
+    );
+    // 2.2.响应拦截
+    axios1.interceptors.response.use(
+        (res) => {
+            // console.log('responsedata', res)
+            return res.data;
+        },
+        (err) => {
+            console.log(err);
+            // Dialog.alert({
+            //     message:'账户不存在！',
+            // })
+        }
+    );
+    // 3.发送真正的网络请求
+    return axios1(config);
 }
